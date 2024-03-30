4b260ca00b2ff8081a0aa8ad5f7e9ba90b8e1e61
Dheerajtupakula <150324781+Dheerajtupakula@users.noreply.github.com>
2024-03-30 16:57:48 +0530

    Create HomeFeatures.js

---
+++ src/components./Data/HomeFeatures.js
@@ -0,0 +1,64 @@
+const HomeFeatures = [
+  {
+    id: "one",
+    featuresImg:
+      "https://www.nolanai.app/_next/image?url=%2Fimages%2FnewFeatures%2F1.webp&w=750&q=75",
+    featuresSpanclass: "material-symbols-rounded",
+    featuresSpan: "drive_file_rename_outline",
+    featuresTitle: "THE INDUSTRY STANDARD SCRIPT WRITING SOFTWARE",
+    featuresPara:
+      "Nolan provides a user-friendly interface, advanced formatting options, and helpful features such as character and scene breakdowns, revision tracking, and collaboration tools.With its powerful tools and intuitive design.",
+  },
+  {
+    id: "two",
+    featuresImg:
+      "https://www.nolanai.app/_next/image?url=%2Fimages%2FnewFeatures%2FplotHole.webp&w=750&q=75",
+    featuresSpanclass: "material-symbols-rounded",
+    featuresSpan: "troubleshoot",
+    featuresTitle: "PLOT HOLE DETECTION REPORT",
+    featuresPara:
+      "With the Plot Hole Detection Report, you can ensure the consistency and quality of your story, enhancing the overall engagement of your content. This detailed report provides actionable insights and suggestions for resolving identified issues, allowing you to fine-tune your narrative and captivate your audience with a more polished and immersive storytelling experience. Whether you're crafting a screenplay, novel, or any other form of narrative, this feature is your indispensable companion in the pursuit of storytelling excellence.",
+  },
+  {
+    id: "three",
+    featuresImg:
+      "https://www.nolanai.app/_next/image?url=%2Fimages%2FnewFeatures%2F3.webp&w=750&q=75",
+    featuresSpanclass: "material-symbols-rounded",
+    featuresSpan: "lightbulb",
+    featuresTitle: "ASK NOLAN FOR SUGGESTION",
+    featuresPara:
+      "To help you create high-quality story, Nolan offers a powerful editing feature. With Ask Nolan, you can easily enhance the quality of your text by highlighting the selected text,correcting any spelling and grammar errors, and adjusting the tone of the paragraph as necessary.This feature provides you with the flexibility to customize your text and make it more engaging and effective.",
+  },
+  {
+    id: "four",
+    featuresImg:
+      "https://www.nolanai.app/_next/image?url=%2Fimages%2FnewFeatures%2F4.webp&w=750&q=75",
+    featuresSpanclass: "material-symbols-rounded",
+    featuresSpan: "translate",
+    featuresTitle: "CHANGE YOUR CHARACTER ACCENT",
+    featuresPara:
+      "Personalize your character and make it truly unique. 'Change Accent' allows you to change your character's accent, making it sound more distinct and authentic. Whether you want to add a touch of personality to your character or create a character that stands out, this feature provides you with the flexibility to customize your character's accent to match your preferences. With this feature, you can create a character that truly represents you and brings your story to life.",
+  },
+  {
+    id: "five",
+    featuresImg:
+      "https://www.nolanai.app/_next/image?url=%2Fimages%2FnewFeatures%2F5.webp&w=750&q=75",
+    featuresSpanclass: "material-symbols-rounded",
+    featuresSpan: "format_align_center",
+    featuresTitle: "SCRIPT SMART",
+    featuresPara:
+      "Script Smart uses advanced algorithms to automatically format your text into a screenplay format. With Script Smart, you can save time and focus on writing by letting the software handle the formatting for you. This feature also provides suggestions and corrections to help ensure that your screenplay adheres toindustry standards and is formatted correctly.",
+  },
+  {
+    id: "six",
+    featuresImg:
+      "https://www.nolanai.app/_next/image?url=%2Fimages%2FnewFeatures%2FscriptBreakdown.webp&w=750&q=75",
+    featuresSpanclass: "material-symbols-rounded",
+    featuresSpan: "list_alt",
+    featuresTitle: "BREAK SMART",
+    featuresPara:
+      "The Automatic Screenplay Breakdown (Break Smart) is a time-saving solution that simplifies the process of deconstructing a screenplay. It automatically dissects your script into key elements like scenes, characters, and locations, providing a clear and organized overview of your story's structure.",
+  },
+];
+
+export default HomeFeatures;
