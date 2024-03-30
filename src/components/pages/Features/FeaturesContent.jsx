cbabd2cc7e896d39e270f9c39913ad1b0dfebaac
Dheerajtupakula <150324781+Dheerajtupakula@users.noreply.github.com>
2024-03-30 17:16:39 +0530

    Create FeaturesContent.jsx

---
+++ src/components/pages/Features/FeaturesContent.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./features.css";
+import featuresData from "../../Data/featuresData";
+function FeaturesContent() {
+  return (
+    <section className="features">
+      <div className="features-bgc-title">
+        <div className="features-bgc">
+          <img
+            src="https://www.nolanai.app/_next/image?url=%2Fimages%2FaboutUs.webp&w=1200&q=75"
+            alt=""
+            className="img-fluid"
+          />
+        </div>
+        <div className="features-titles">
+          <h1 className="features-title">FEATURES</h1>
+          <h3 className="features-title-dec">
+            See what NolanAI comprehensive toolkit can do for you.
+          </h3>
+        </div>
+      </div>
+      <div className="features-contents">
+        {featuresData.map((feature) => {
+          return (
+            <div key={feature.id} className="features-content">
+              <div className="features-content-flex">
+                <div className={`features-content-img ${feature.id}`}>
+                  <span className="material-symbols-rounded">
+                    {feature.feature_logo}
+                  </span>
+                </div>
+                <div className="features-content-title-para">
+                  <h2 className="features-content-title">
+                    {feature.feature_Title}
+                  </h2>
+                  <p className="features-content-para">
+                    {feature.feature_para}
+                  </p>
+                </div>
+              </div>
+              <a href={feature.feature_to_btn}>Explore Feature</a>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default FeaturesContent;
