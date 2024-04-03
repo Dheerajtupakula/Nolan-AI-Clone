66a39395332b28e79d866da7f65b84d11d634e08
Dheerajtupakula <150324781+Dheerajtupakula@users.noreply.github.com>
2024-04-03 17:28:10 +0530

    Create MapBlog.js

---
+++ src/components/pages/Blog/MapBlog.js
@@ -0,0 +1,364 @@
+import AICopilotforScriptwriters from "./Blog-Articles/AICopilotforScriptwriters";
+import AIinFilmandTVAHarmoniousPartnershipwithHumanCreativity from "./Blog-Articles/AIinFilmandTVAHarmoniousPartnershipwithHumanCreativity";
+import ActionFilmScriptsThatTakePlaceDuringChristmas from "./Blog-Articles/ActionFilmScriptsThatTakePlaceDuringChristmas";
+import BoostYourScriptWritingSkillswithTheseTop3Podcasts from "./Blog-Articles/BoostYourScriptWritingSkillswithTheseTop3Podcasts";
+import BreakingBarriersandMakingHistoryTheOscars from "./Blog-Articles/BreakingBarriersandMakingHistoryTheOscars";
+import CelebratingtheCreativeBrillianceWomenScriptwritersShapingtheNarrative from "./Blog-Articles/CelebratingtheCreativeBrillianceWomenScriptwritersShapingtheNarrative";
+import CollaborativeScriptwritingUnveilingRealTimeFeedbackSharing from "./Blog-Articles/CollaborativeScriptwritingUnveilingRealTimeFeedbackSharing";
+import CraftingCompellingConversationsTheArtofMovieDialogues from "./Blog-Articles/CraftingCompellingConversationsTheArtofMovieDialogues";
+import CraftingMemorableClosingsAGuidetoMasteringtheArtofScriptEndings from "./Blog-Articles/CraftingMemorableClosingsAGuidetoMasteringtheArtofScriptEndings";
+import DecodingMidpointMagicinScriptwriting from "./Blog-Articles/DecodingMidpointMagicinScriptwriting";
+import DemocratizingScreenwriting from "./Blog-Articles/DemocratizingScreenwriting";
+import DivingintotheBarbieMovieScriptwritingEnchantment from "./Blog-Articles/DivingintotheBarbieMovieScriptwritingEnchantment";
+import EasyStepsToMasteringScriptBreakdown from "./Blog-Articles/EasyStepsToMasteringScriptBreakdown";
+import EnhancingtheScreenwritingExperienceUnveilingExcitingNewFeaturesinNolanAI from "./Blog-Articles/EnhancingtheScreenwritingExperienceUnveilingExcitingNewFeaturesinNolanAI";
+import ExploringNewWorldsHowtoWriteaSciFiScreenplay from "./Blog-Articles/ExploringNewWorldsHowtoWriteaSciFiScreenplay";
+import FeedbackMattersEnhancingYourScreenwritingJourney from "./Blog-Articles/FeedbackMattersEnhancingYourScreenwritingJourney";
+import FinalDraftBigBreak2023EnterNow from "./Blog-Articles/FinalDraftBigBreak2023EnterNow";
+import FreeScriptingPowerCeltxWriterDuetNolanAIUnleashed from "./Blog-Articles/FreeScriptingPowerCeltxWriterDuetNolanAIUnleashed";
+import GuidelinesforScreenplayWriting from "./Blog-Articles/GuidelinesforScreenplayWriting";
+import HarmonyinHollywoodAVictoryforCreativityandAIsRoleinEntertainment from "./Blog-Articles/HarmonyinHollywoodAVictoryforCreativityandAIsRoleinEntertainment";
+import HowLongShouldaScreenplayBe from "./Blog-Articles/HowLongShouldaScreenplayBe";
+import HowMovieTrailersareMadeABehindtheScenesLook from "./Blog-Articles/HowMovieTrailersareMadeABehindtheScenesLook";
+import HowNottoWriteaScreenplay from "./Blog-Articles/HowNottoWriteaScreenplay";
+import HowProducersHarnessNolanAIScriptCoverageforSuccess from "./Blog-Articles/HowProducersHarnessNolanAIScriptCoverageforSuccess";
+import HowtoBecomeaBetterScreenwriter from "./Blog-Articles/HowtoBecomeaBetterScreenwriter";
+import HowtoMakeASceneShorter from "./Blog-Articles/HowtoMakeASceneShorter";
+import JointheNolanAIScriptCampaignandUnleashYourCreativePotential from "./Blog-Articles/JointheNolanAIScriptCampaignandUnleashYourCreativePotential";
+import LeveragingScriptBreakdownforEffectiveFilmScheduling from "./Blog-Articles/LeveragingScriptBreakdownforEffectiveFilmScheduling";
+import MasteringHollywoodstyleScriptwritingwithScriptfellaProgram from "./Blog-Articles/MasteringHollywoodstyleScriptwritingwithScriptfellaProgram";
+import MasteringSceneManagementinNolanYourComprehensiveGuide from "./Blog-Articles/MasteringSceneManagementinNolanYourComprehensiveGuide";
+import NolanAIProPlansNewFeaturePlotHoleReportsforEnhancedStorytelling from "./Blog-Articles/NolanAIProPlansNewFeaturePlotHoleReportsforEnhancedStorytelling";
+import NolanAITikTokInstagramScriptCrafters from "./Blog-Articles/NolanAITikTokInstagramScriptCrafters";
+import NovelScreenplayTheSecretsofStorytellingSuccess from "./Blog-Articles/NovelScreenplayTheSecretsofStorytellingSuccess";
+import OppenheimerScript from "./Blog-Articles/OppenheimerScript";
+import ScreenwritingTerminologyFormatting from "./Blog-Articles/ScreenwritingTerminologyFormatting";
+import ScriptCoverageBestPracticesforTheBigScreen from "./Blog-Articles/ScriptCoverageBestPracticesforTheBigScreen";
+import SundanceACinematicOdyssey from "./Blog-Articles/SundanceACinematicOdyssey";
+import TheCinematicCanvasImportanceofStoryboardsinFilmProduction from "./Blog-Articles/TheCinematicCanvasImportanceofStoryboardsinFilmProduction";
+import TheFriendsinLeavetheWorldBehind from "./Blog-Articles/TheFriendsinLeavetheWorldBehind";
+import TheHeartbeataStrongOpeningScene from "./Blog-Articles/TheHeartbeataStrongOpeningScene";
+import TheImpactofAIonFilmProduction from "./Blog-Articles/TheImpactofAIonFilmProduction";
+import TheSceneBeatSheetStreamliningScreenplayWritingwithNolanAI from "./Blog-Articles/TheSceneBeatSheetStreamliningScreenplayWritingwithNolanAI";
+import TheTop10MistakesNewScreenwritersMakeAndHowtoAvoidThem from "./Blog-Articles/TheTop10MistakesNewScreenwritersMakeAndHowtoAvoidThem";
+import ThrillingOpportunitiesAGuidetoThrillerFestforWritersandFans from "./Blog-Articles/ThrillingOpportunitiesAGuidetoThrillerFestforWritersandFans";
+import UnderstandingtheComplexityofFilmScheduling from "./Blog-Articles/UnderstandingtheComplexityofFilmScheduling";
+import UnravelingtheArtofPlotHoleDetectionandResolvingNarrativeTangles from "./Blog-Articles/UnravelingtheArtofPlotHoleDetectionandResolvingNarrativeTangles";
+import UntamedStoriesIndieFilmmakingUnveiled from "./Blog-Articles/UntamedStoriesIndieFilmmakingUnveiled";
+import UnveilingtheMostWantedMoviesof2024 from "./Blog-Articles/UnveilingtheMostWantedMoviesof2024";
+import UnveilingtheStagesandCostsofFilmDevelopment from "./Blog-Articles/UnveilingtheStagesandCostsofFilmDevelopment";
+import WhatIsaScreenplay from "./Blog-Articles/WhatIsaScreenplay";
+import WhatisScreenplayBreakdown from "./Blog-Articles/WhatisScreenplayBreakdown";
+import WhatisSpecScript from "./Blog-Articles/WhatisSpecScript";
+import WomensImpactBehindTheScenesTheFilmIndustry from "./Blog-Articles/WomensImpactBehindTheScenesTheFilmIndustry";
+import WriteTogetherCreateMagicExploreColLabTheVirtualWritersRoom from "./Blog-Articles/WriteTogetherCreateMagicExploreColLabTheVirtualWritersRoom";
+import WritinganUnforgettableActionScreenplayWhatYouNeedtoKnow from "./Blog-Articles/WritinganUnforgettableActionScreenplayWhatYouNeedtoKnow";
+import WritingforImpactHowtoWriteaDramaticScreenplayThatStickswithYourAudience from "./Blog-Articles/WritingforImpactHowtoWriteaDramaticScreenplayThatStickswithYourAudience";
+
+const MapBlog = [
+  {
+    id: "/blog/9-easy-steps-to-mastering-script-breakdowns-in-filmmaking",
+    path: "/blog/9-easy-steps-to-mastering-script-breakdowns-in-filmmaking",
+    path_element: <EasyStepsToMasteringScriptBreakdown />,
+  },
+  {
+    id: "/blog/breaking-barriers-and-making-history-at-the-oscars",
+    path: "/blog/breaking-barriers-and-making-history-at-the-oscars",
+    path_element: <BreakingBarriersandMakingHistoryTheOscars />,
+  },
+  {
+    id: "/blog/womens-impact-behind-the-scenes-in-the-film-industry",
+    path: "/blog/womens-impact-behind-the-scenes-in-the-film-industry",
+    path_element: <WomensImpactBehindTheScenesTheFilmIndustry />,
+  },
+  {
+    id: "/blog/how-producers-harness-nolanai-script-coverage-for-success",
+    path: "/blog/how-producers-harness-nolanai-script-coverage-for-success",
+    path_element: <HowProducersHarnessNolanAIScriptCoverageforSuccess />,
+  },
+  {
+    id: "/blog/script-coverage-best-practices-for-the-big-screen",
+    path: "/blog/script-coverage-best-practices-for-the-big-screen",
+    path_element: <ScriptCoverageBestPracticesforTheBigScreen />,
+  },
+  {
+    id: "/blog/untamed-stories-indie-filmmaking-unveiled",
+    path: "/blog/untamed-stories-indie-filmmaking-unveiled",
+    path_element: <UntamedStoriesIndieFilmmakingUnveiled />,
+  },
+  {
+    id: "/blog/how-to-make-my-scene-shorter",
+    path: "/blog/how-to-make-my-scene-shorter",
+    path_element: <HowtoMakeASceneShorter />,
+  },
+  {
+    id: "/blog/free-scripting-power-celtx-writerduet-nolanai-unleashed",
+    path: "/blog/free-scripting-power-celtx-writerduet-nolanai-unleashed",
+    path_element: <FreeScriptingPowerCeltxWriterDuetNolanAIUnleashed />,
+  },
+  {
+    id: "/blog/2024-sundance-a-cinematic-odyssey",
+    path: "/blog/2024-sundance-a-cinematic-odyssey",
+    path_element: <SundanceACinematicOdyssey />,
+  },
+  {
+    id: "/blog/crafting-compelling-conversations-the-art-of-movie-dialogues",
+    path: "/blog/crafting-compelling-conversations-the-art-of-movie-dialogues",
+    path_element: <CraftingCompellingConversationsTheArtofMovieDialogues />,
+  },
+  {
+    id: "/blog/unveiling-the-most-wanted-movies-of-2024",
+    path: "/blog/unveiling-the-most-wanted-movies-of-2024",
+    path_element: <UnveilingtheMostWantedMoviesof2024 />,
+  },
+  {
+    id: "/blog/leveraging-script-breakdown-for-effective-film-scheduling",
+    path: "/blog/leveraging-script-breakdown-for-effective-film-scheduling",
+    path_element: <LeveragingScriptBreakdownforEffectiveFilmScheduling />,
+  },
+  {
+    id: "/blog/how-movie-trailers-are-made",
+    path: "/blog/how-movie-trailers-are-made",
+    path_element: <HowMovieTrailersareMadeABehindtheScenesLook />,
+  },
+  {
+    id: "/blog/understanding-the-complexity-of-film-scheduling",
+    path: "/blog/understanding-the-complexity-of-film-scheduling",
+    path_element: <UnderstandingtheComplexityofFilmScheduling />,
+  },
+  {
+    id: "/blog/the-friends-in-leave-the-world-behind",
+    path: "/blog/the-friends-in-leave-the-world-behind",
+    path_element: <TheFriendsinLeavetheWorldBehind />,
+  },
+  {
+    id: "/blog/novel-to-screenplay-the-secrets-of-storytelling-success",
+    path: "/blog/novel-to-screenplay-the-secrets-of-storytelling-success",
+    path_element: <NovelScreenplayTheSecretsofStorytellingSuccess />,
+  },
+  {
+    id: "/blog/how-not-to-write-a-screenplay",
+    path: "/blog/how-not-to-write-a-screenplay",
+    path_element: <HowNottoWriteaScreenplay />,
+  },
+  {
+    id: "/blog/action-packed-christmas-5-action-film-scripts-that-take-place-during",
+    path: "/blog/action-packed-christmas-5-action-film-scripts-that-take-place-during",
+    path_element: <ActionFilmScriptsThatTakePlaceDuringChristmas />,
+  },
+  {
+    id: "/blog/stages-and-costs-of-film-development",
+    path: "/blog/stages-and-costs-of-film-development",
+    path_element: <UnveilingtheStagesandCostsofFilmDevelopment />,
+  },
+  {
+    id: "/blog/screenwriting-terminology-formatting",
+    path: "/blog/screenwriting-terminology-formatting",
+    path_element: <ScreenwritingTerminologyFormatting />,
+  },
+  {
+    id: "/blog/crafting-memorable-closings-a-guide-to-mastering-the-art-of-script-endings",
+    path: "/blog/crafting-memorable-closings-a-guide-to-mastering-the-art-of-script-endings",
+    path_element: (
+      <CraftingMemorableClosingsAGuidetoMasteringtheArtofScriptEndings />
+    ),
+  },
+  {
+    id: "/blog/oppenheimer-script",
+    path: "/blog/oppenheimer-script",
+    path_element: <OppenheimerScript />,
+  },
+  {
+    id: "/blog/decoding-midpoint-magic-in-scriptwriting",
+    path: "/blog/decoding-midpoint-magic-in-scriptwriting",
+    path_element: <DecodingMidpointMagicinScriptwriting />,
+  },
+  {
+    id: "/blog/the-heartbeat-of-a-strong-opening-scene",
+    path: "/blog/the-heartbeat-of-a-strong-opening-scene",
+    path_element: <TheHeartbeataStrongOpeningScene />,
+  },
+  {
+    id: "/blog/impact-of-ai-on-film-production",
+    path: "/blog/impact-of-ai-on-film-production",
+    path_element: <TheImpactofAIonFilmProduction />,
+  },
+  {
+    id: "/blog/the-cinematic-canvas-importance-of-storyboards-in-film-production",
+    path: "/blog/the-cinematic-canvas-importance-of-storyboards-in-film-production",
+    path_element: <TheCinematicCanvasImportanceofStoryboardsinFilmProduction />,
+  },
+  {
+    id: "/blog/feedback-matters-enhancing-your-screenwriting-journey",
+    path: "/blog/feedback-matters-enhancing-your-screenwriting-journey",
+    path_element: <FeedbackMattersEnhancingYourScreenwritingJourney />,
+  },
+  {
+    id: "/blog/harmony-in-hollywood-a-victory-for-creativity-and-ais-role-in-entertainment",
+    path: "/blog/harmony-in-hollywood-a-victory-for-creativity-and-ais-role-in-entertainment",
+    path_element: (
+      <HarmonyinHollywoodAVictoryforCreativityandAIsRoleinEntertainment />
+    ),
+  },
+  {
+    id: "/blog/nolanai-pro-plans-new-feature-plot-hole-reports-for-enhanced-storytelling",
+    path: "/blog/nolanai-pro-plans-new-feature-plot-hole-reports-for-enhanced-storytelling",
+    path_element: (
+      <NolanAIProPlansNewFeaturePlotHoleReportsforEnhancedStorytelling />
+    ),
+  },
+  {
+    id: "/blog/write-together-create-magic-explore-collab-the-virtual-writers-room",
+    path: "/blog/write-together-create-magic-explore-collab-the-virtual-writers-room",
+    path_element: (
+      <WriteTogetherCreateMagicExploreColLabTheVirtualWritersRoom />
+    ),
+  },
+  {
+    id: "/blog/ai-in-film-and-tv-a-harmonious-partnership-with-human-creativity",
+    path: "/blog/ai-in-film-and-tv-a-harmonious-partnership-with-human-creativity",
+    path_element: <AIinFilmandTVAHarmoniousPartnershipwithHumanCreativity />,
+  },
+  {
+    id: "/blog/nolanai-tiktok-and-instagram-scriptcrafters",
+    path: "/blog/nolanai-tiktok-and-instagram-scriptcrafters",
+    path_element: <NolanAITikTokInstagramScriptCrafters />,
+  },
+  {
+    id: "/blog/diving-into-the-barbie-movie-scriptwriting-enchantment",
+    path: "/blog/diving-into-the-barbie-movie-scriptwriting-enchantment",
+    path_element: <DivingintotheBarbieMovieScriptwritingEnchantment />,
+  },
+  {
+    id: "/blog/mastering-scene-management-in-nolan-your-comprehensive-guide",
+    path: "/blog/mastering-scene-management-in-nolan-your-comprehensive-guide",
+    path_element: <MasteringSceneManagementinNolanYourComprehensiveGuide />,
+  },
+  {
+    id: "/blog/collaborative-scriptwriting-unveiling-real-time-feedback-sharing",
+    path: "/blog/collaborative-scriptwriting-unveiling-real-time-feedback-sharing",
+    path_element: (
+      <CollaborativeScriptwritingUnveilingRealTimeFeedbackSharing />
+    ),
+  },
+  {
+    id: "/blog/unraveling-the-art-of-plot-hole-detection-and-resolving-narrative-tangles",
+    path: "/blog/unraveling-the-art-of-plot-hole-detection-and-resolving-narrative-tangles",
+    path_element: (
+      <UnravelingtheArtofPlotHoleDetectionandResolvingNarrativeTangles />
+    ),
+  },
+  {
+    id: "/blog/celebrating-the-creative-brilliance-women-scriptwriters-shaping-the",
+    path: "/blog/celebrating-the-creative-brilliance-women-scriptwriters-shaping-the",
+    path_element: (
+      <CelebratingtheCreativeBrillianceWomenScriptwritersShapingtheNarrative />
+    ),
+  },
+  {
+    id: "/blog/the-scene-beat-sheet-streamlining-screenplay-writing-with-nolanai",
+    path: "/blog/the-scene-beat-sheet-streamlining-screenplay-writing-with-nolanai",
+    path_element: <TheSceneBeatSheetStreamliningScreenplayWritingwithNolanAI />,
+  },
+  {
+    id: "/blog/enhancing-the-screenwriting-experience-unveiling-exciting-new-features-in",
+    path: "/blog/enhancing-the-screenwriting-experience-unveiling-exciting-new-features-in",
+    path_element: (
+      <EnhancingtheScreenwritingExperienceUnveilingExcitingNewFeaturesinNolanAI />
+    ),
+  },
+  {
+    id: "/blog/final-draft-big-break-2023-enter-now",
+    path: "/blog/final-draft-big-break-2023-enter-now",
+    path_element: <FinalDraftBigBreak2023EnterNow />,
+  },
+  {
+    id: "/blog/what-is-screenplay-breakdown",
+    path: "/blog/what-is-screenplay-breakdown",
+    path_element: <WhatisScreenplayBreakdown />,
+  },
+  {
+    id: "/blog/join-the-nolanaiscript-campaign-and-unleash-your-creative-potential",
+    path: "/blog/join-the-nolanaiscript-campaign-and-unleash-your-creative-potential",
+    path_element: (
+      <JointheNolanAIScriptCampaignandUnleashYourCreativePotential />
+    ),
+  },
+  {
+    id: "/blog/mastering-hollywood-style-scriptwriting-with-scriptfella-program",
+    path: "/blog/mastering-hollywood-style-scriptwriting-with-scriptfella-program",
+    path_element: (
+      <MasteringHollywoodstyleScriptwritingwithScriptfellaProgram />
+    ),
+  },
+  {
+    id: "/blog/boost-your-script-writing-skills-with-these-top-3-podcasts",
+    path: "/blog/boost-your-script-writing-skills-with-these-top-3-podcasts",
+    path_element: <BoostYourScriptWritingSkillswithTheseTop3Podcasts />,
+  },
+  {
+    id: "/blog/democratizing-screenwriting",
+    path: "/blog/democratizing-screenwriting",
+    path_element: <DemocratizingScreenwriting />,
+  },
+  {
+    id: "/blog/a-guide-to-thriller-fest-for-writers-and-fans",
+    path: "/blog/a-guide-to-thriller-fest-for-writers-and-fans",
+    path_element: (
+      <ThrillingOpportunitiesAGuidetoThrillerFestforWritersandFans />
+    ),
+  },
+  {
+    id: "/blog/guidelines-for-screenplay-writing",
+    path: "/blog/guidelines-for-screenplay-writing",
+    path_element: <GuidelinesforScreenplayWriting />,
+  },
+  {
+    id: "/blog/what-is-spec-script",
+    path: "/blog/what-is-spec-script",
+    path_element: <WhatisSpecScript />,
+  },
+  {
+    id: "/blog/ai-copilot-for-scriptwriters",
+    path: "/blog/ai-copilot-for-scriptwriters",
+    path_element: <AICopilotforScriptwriters />,
+  },
+  {
+    id: "/blog/writing-an-unforgettable-action-screenplay-what-you-need-to-know",
+    path: "/blog/writing-an-unforgettable-action-screenplay-what-you-need-to-know",
+    path_element: <WritinganUnforgettableActionScreenplayWhatYouNeedtoKnow />,
+  },
+  {
+    id: "/blog/exploring-new-worlds-how-to-write-a-sci-fi-screenplay",
+    path: "/blog/exploring-new-worlds-how-to-write-a-sci-fi-screenplay",
+    path_element: <ExploringNewWorldsHowtoWriteaSciFiScreenplay />,
+  },
+  {
+    id: "/blog/how-to-write-a-dramatic-screenplay-that-sticks-with-your-audience",
+    path: "/blog/how-to-write-a-dramatic-screenplay-that-sticks-with-your-audience",
+    path_element: (
+      <WritingforImpactHowtoWriteaDramaticScreenplayThatStickswithYourAudience />
+    ),
+  },
+  {
+    id: "/blog/how-to-become-a-better-screenwriter",
+    path: "/blog/how-to-become-a-better-screenwriter",
+    path_element: <HowtoBecomeaBetterScreenwriter />,
+  },
+  {
+    id: "/blog/the-top-10-mistakes-new-screenwriters-make-and-how-to-avoid-them",
+    path: "/blog/the-top-10-mistakes-new-screenwriters-make-and-how-to-avoid-them",
+    path_element: <TheTop10MistakesNewScreenwritersMakeAndHowtoAvoidThem />,
+  },
+  {
+    id: "/blog/how-long-should-a-screenplay-be",
+    path: "/blog/how-long-should-a-screenplay-be",
+    path_element: <HowLongShouldaScreenplayBe />,
+  },
+  {
+    id: "/blog/what-is-a-screenplay",
+    path: "/blog/what-is-a-screenplay",
+    path_element: <WhatIsaScreenplay />,
+  },
+];
+export default MapBlog;
