d5a3deeec08db2201c92b723cca6508e58cc657c
Dheerajtupakula <150324781+Dheerajtupakula@users.noreply.github.com>
2024-04-03 17:30:52 +0530

    Create featuresData.js

---
+++ src/components/Data/featuresData.js
@@ -0,0 +1,97 @@
+const featuresData = [
+  {
+    id: "one",
+    feature_logo: "movie_filter",
+    feature_Title: "AI Deck Builder- Coming Soon",
+    feature_para:
+      "The essential tool for filmmakers seeking to craft captivating pitch decks effortlessly.",
+    feature_to_btn: "/features/pitch-deck",
+  },
+  {
+    id: "two",
+    feature_logo: "newspaper",
+    feature_Title: "AI SCRIPT COVERAGE REPORTING",
+    feature_para:
+      "AI Script Coverage delivers a comprehensive report on your script's logline, impression, synopsis, and character details—all within minutes.",
+    feature_to_btn: "/features/ai-script-coverage-reporting",
+  },
+  {
+    id: "three",
+    feature_logo: "savings",
+    feature_Title: "AI BUDGETING",
+    feature_para: "Coming Soon",
+    feature_to_btn: "/features/ai-budgeting",
+  },
+  {
+    id: "four",
+    feature_logo: "schedule",
+    feature_Title: "SCHEDULING",
+    feature_para:
+      "Automatically sorts your stripboard (with advanced algorithms and the power of AI) in the most efficient way for shooting.",
+    feature_to_btn: "/features/scheduling",
+  },
+  {
+    id: "five",
+    feature_logo: "psychology",
+    feature_Title: "BRAINSTORMING",
+    feature_para:
+      "Collaborate with AI on brainstorming for script and screenplay writing.",
+    feature_to_btn: "/features/brainstorming",
+  },
+  {
+    id: "six",
+    feature_logo: "developer_board",
+    feature_Title: "STORYBOARD",
+    feature_para: "Next-Level Storyboard for Film and Story Development",
+    feature_to_btn: "/features/storyboard",
+  },
+  {
+    id: "seven",
+    feature_logo: "video_camera_front",
+    feature_Title: "CHARACTER DEVELOPMENT",
+    feature_para: "Create, Edit your character with help of AI. ",
+    feature_to_btn: "/features/character-development",
+  },
+  {
+    id: "eight",
+    feature_logo: "auto_awesome",
+    feature_Title: "AI COPILOT",
+    feature_para:
+      "To help you create high-quality story, Nolan offers a powerful  AI editing feature.",
+    feature_to_btn: "/features/ai-copilot",
+  },
+  {
+    id: "nine",
+    feature_logo: "format_align_center",
+    feature_Title: "SMART FORMATTING",
+    feature_para:
+      "Script Smart uses advanced algorithms to automatically format your text into a screenplay format.",
+    feature_to_btn: "/features/smart-formatting",
+  },
+  {
+    id: "ten",
+    feature_logo: "list_alt",
+    feature_Title: "AUTOMATIC SCRIPT BREAKDOWN",
+    feature_para:
+      "The Automatic Screenplay Breakdown (Break Smart) is a time-saving solution that simplifies the process of deconstructing a screenplay.",
+    feature_to_btn: "/features/automatic-script-breakdown",
+  },
+  {
+    id: "eleven",
+    feature_logo: "troubleshoot",
+    feature_Title: "PLOT HOLE DETECTION REPORT",
+    feature_para:
+      "With the Plot Hole Detection Report, you can ensure the consistency and quality of your story, enhancing the overall engagement of your content.",
+    feature_to_btn: "/features/plot-hole-detection-report",
+  },
+  {
+    id: "twelve",
+    feature_logo: "drive_file_rename_outline",
+    feature_Title: "THE INDUSTRY STANDARD EDITOR",
+    feature_para:
+      "Nolan provides a user-friendly interface, advanced formatting options.",
+    feature_to_btn: "/features/the-industry-standard-editor",
+  },
+];
+
+export default featuresData;
